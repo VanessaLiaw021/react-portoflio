9ae1e6548ec9c22a4a6f6f4719f26d5f9ceee81a
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-21 12:42:20 -0400

    created a component called Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+//Import packages
+import React from 'react';
+
+//Header Component 
+const Header = () => {
+  return (
+    <div>
+        <h1>Vanessa Liaw</h1>
+    </div>
+  )
+}
+
+export default Header;
