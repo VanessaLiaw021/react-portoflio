53200acfa042cb41cdc69ce85425dd5081f724a8
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-24 12:19:32 -0400

    added all required component so I can link them when clicked

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+//Import packages
+import React from 'react';
+
+//Contact Component
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+//Export Contact
+export default Contact;
---
+++ src/components/Resume.js
@@ -0,0 +1,12 @@
+//Import packages
+import React from 'react';
+
+//Resume Component
+const Resume = () => {
+  return (
+    <div>Resume</div>
+  )
+}
+
+//Export Resume Component
+export default Resume;
