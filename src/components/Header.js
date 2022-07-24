b7fcef764d46a7a4f99db22ed9e63e38022af748
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-24 11:08:55 -0400

    added a nav bar in the nav compoonent

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 //Import packages
 import React from 'react';
+import Nav from "./Nav";
 
 //Header Component 
 const Header = () => {
   return (
     <div>
         <h1>Vanessa Liaw</h1>
+        <Nav />
     </div>
   )
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+        <nav>
+            <a href="/home">Home</a>
+            <a href="/portoflio">Portfolio</a>
+            <a href="/contact">Contact</a>
+            <a href="/resume">Resume</a>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav;
