7bdd81e39885e7771397ba3b5ce3edf855f01a83
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-24 12:11:53 -0400

    added projects component content

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+//Import Packages
+import React from 'react';
+
+//Project Component
+const Project = () => {
+  return (
+    <div>Project</div>
+  )
+}
+
+//Export Project
+export default Project;
