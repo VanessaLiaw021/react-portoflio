8f14d466e6ffc7e8cc33d845e79f6457496be8ad
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-24 12:10:14 -0400

    added portfolio component and import in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <hr></hr>
       <About />
+      <Portfolio />
     </>
   );
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+//Import packages
+import React from 'react';
+
+//Portfolio Component
+const Portfolio = () => {
+  return (
+    <div>Portfolio</div>
+  )
+}
+
+//Export Portoflio Component
+export default Portfolio;
