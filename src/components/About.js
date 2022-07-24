8e3ec7d3b5c8e9c4b9e2af84b209e78ec7726ff5
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-24 12:08:09 -0400

    added image, and about me section in the about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 //Import packages
 import React from 'react';
+import Avatar from "../assets/images/avatar.jpg";
 
 //About Component
 const About = () => {
@@ -9,10 +10,17 @@ const About = () => {
 
         <div class="about">
             <div>
-                <img></img>
+                <img src={Avatar} alt="Images of Vanessa Liaw" className="avatar"/>
             </div>
             <div>
-                <h3>Hi, I'm Vanessa Liaw</h3>
+                <h3 className="about-heading">Hi, I'm <span>Vanessa Liaw</span></h3>
+                <p className="about-me">I am 23 years old and I was born on August 4, 1998. I graduated with a 
+                    Bachelor in <br></br> Computer Science and concentrating in Information Technology. I also minor
+                    in <br></br> mathematics. I am currently pursuing my career as a Front End Developer because 
+                    <br></br> I like designing website for others to use and enjoyed. My favorite hobbies are 
+                    traveling <br></br> anywhere in the world, listening to music, and watch television 
+                    (especially chinese dramas <br></br> and korean dramas).
+                </p>
             </div>
         </div>
     </div>
