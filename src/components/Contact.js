c005ea475c4b5dcad52acb87acc183ede83843b2
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-25 10:52:31 -0400

    created handleSubmit and handleChange for the form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,35 @@
 //Import packages
 import React, { useState } from 'react';
+import validateEmail from "../utils/helpers";
 
 //Contact Component
 const Contact = () => {
 
   //Create state variables and set them to empty string 
   const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  //Function that handle form change 
+  const handleChange = (e) => {
+
+    //Get the name and value of the input 
+    const value = e.target.value;
+    const name = e.target.name;
+
+  };
+
+  //Function that handle form submit
+  const handleSubmit = (e) => {
+
+    //Prevent default action 
+    e.preventDefault();
+
+    //Clear the form 
+    setFullName("");
+    setEmail("");
+    setMessage("");
+  }
 
   return (
     <div className="main">
@@ -15,17 +38,17 @@ const Contact = () => {
       <form>
         <div className="form-group">
           <label>Full Name:</label>
-          <input value={fullName} name="Full Name" type="text" placeholder="Full Name"></input>
+          <input value={fullName} name="Full Name" type="text" placeholder="Full Name" onChange={handleChange}></input>
         </div>
         <div className="form-group">
           <label>Email:</label>
-          <input value={email} name="Email" type="text" placeholder="Email"></input>
+          <input value={email} name="Email" type="text" placeholder="Email" onChange={handleChange}></input>
         </div>
         <div className="form-group">
           <label>Message:</label>
           <textarea></textarea>
         </div>
-        <button className="send">Send</button>
+        <button className="send" onSubmit={handleSubmit}>Send</button>
       </form>
     </div>
   );
