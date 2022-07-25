d1947b9b5c99bb11d7565b961996c26d5d9bb599
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-25 19:24:32 -0400

    trying to fix the errors in portfolio component and project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,14 +4,13 @@ import React from 'react';
 //Project Component
 const Project = (props) => {
   return (
-    <div className="card">
-      <div className="card-wrapper">
+    <div>
+      <div className="card-wrapper" key={props.id}>
         <div className="card-image">
           <img src={props.image} alt={props.imageAlt}/>
         </div>
         <div className="card-content">
           <p>{props.title}</p>
-
           <div className="live-github-link">
             <a href={props.liveSite} target="_blank" rel="noreferrer">Live Site</a>
             <a href={props.gitHub} target="_blank" rel="noreferrer">Github</a>
