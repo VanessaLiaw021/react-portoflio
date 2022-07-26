266985d9849506d1d788d4550e15744e3dbee571
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-25 21:23:53 -0400

    added handleChange and handleSubmit for submiiting form and display errors message

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,17 +6,32 @@ import validateEmail from "../utils/helpers";
 const Contact = () => {
 
   //Create state variables and set them to empty string 
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const [formInput, setFormInput] = useState({name: "", email: "", message: ""});
+  const [errorMessage, setErrorMessage] = useState("");
+
+  //Grab only the variable we want to use
+  const { name, email, message } = formInput;
 
   //Function that handle form change 
   const handleChange = (e) => {
 
-    //Get the name and value of the input 
-    const value = e.target.value;
-    const name = e.target.name;
+    //Check to see the field for email 
+    if (e.target.name === "email") {
+      
+      //Use helper method to validate the email
+      const validEmail = validateEmail(e.target.value);
 
+      //Check to see if the email matches the helper method 
+      !validEmail ? setErrorMessage("Enter a valid email") : setErrorMessage("");
+
+    } else {
+
+      !e.target.value.length ? setErrorMessage(`${e.target.name} is required.`) : setErrorMessage("");
+
+    };
+    
+    //Set the target name, email, message if no error occurs
+    if (!errorMessage) setFormInput({...formInput, [e.target.name]: [e.target.value]});
   };
 
   //Function that handle form submit
@@ -25,30 +40,28 @@ const Contact = () => {
     //Prevent default action 
     e.preventDefault();
 
-    //Clear the form 
-    setFullName("");
-    setEmail("");
-    setMessage("");
+    //Clear the Form
+    setFormInput("");
   }
 
   return (
     <div className="main">
       <h2 className="nav-heading">Contact</h2>
-
       <form>
         <div className="form-group">
           <label>Full Name:</label>
-          <input value={fullName} name="Full Name" type="text" placeholder="Full Name" onChange={handleChange}></input>
+          <input name="name" defaultValue={name} type="text" placeholder="Full Name" onBlur={handleChange}></input>
         </div>
         <div className="form-group">
           <label>Email:</label>
-          <input value={email} name="Email" type="text" placeholder="Email" onChange={handleChange}></input>
+          <input name="email" defaultValue={email} type="text" placeholder="Email" onBlur={handleChange}></input>
         </div>
         <div className="form-group">
           <label>Message:</label>
-          <textarea></textarea>
+          <textarea name="message" defaultValue={message}></textarea>
         </div>
-        <button className="send" onSubmit={handleSubmit}>Send</button>
+        <p className="error-message">{errorMessage}</p>
+        <button type="submit" className="send" onSubmit={handleSubmit}>Send</button>
       </form>
     </div>
   );
