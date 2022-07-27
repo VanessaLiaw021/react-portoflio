39e4d0662967937492f4986c8fd150fe0caad6c5
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-26 20:35:09 -0400

    fix footer in about component, added a stack overflow link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,20 @@
 //Import packages
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithubSquare, faLinkedin, faStackOverflow } from "@fortawesome/free-brands-svg-icons";
 
 //Footer Component
 const Footer = () => {
   return (
     <footer>
       <a href="https://github.com/VanessaLiaw021">
-        <FontAwesomeIcon icon={faGithubSquare} className="icon"/>
+        <FontAwesomeIcon icon={faGithubSquare} target="_blank" rel="noreferrer" className="icon"/>
       </a>
       <a href="https://www.linkedin.com/in/vanessa-liaw-b0a1981a2/">
-        <FontAwesomeIcon icon={faLinkedin} className="icon"/>
+        <FontAwesomeIcon icon={faLinkedin} target="_blank" rel="noreferrer" className="icon"/>
+      </a>
+      <a href="https://stackoverflow.com/users/19628967/vliaw">
+        <FontAwesomeIcon icon={faStackOverflow} target="_blank" rel="noreferrer" className="icon"/>
       </a>
     </footer>
   );
