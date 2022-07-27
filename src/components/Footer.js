0e3846e991f55263e19bcabaa0742a46ef687124
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-26 20:38:17 -0400

    added the target=_blank in wrong place, move it to the correct place

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ import { faGithubSquare, faLinkedin, faStackOverflow } from "@fortawesome/free-b
 const Footer = () => {
   return (
     <footer>
-      <a href="https://github.com/VanessaLiaw021">
-        <FontAwesomeIcon icon={faGithubSquare} target="_blank" rel="noreferrer" className="icon"/>
+      <a href="https://github.com/VanessaLiaw021" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faGithubSquare} className="icon"/>
       </a>
-      <a href="https://www.linkedin.com/in/vanessa-liaw-b0a1981a2/">
-        <FontAwesomeIcon icon={faLinkedin} target="_blank" rel="noreferrer" className="icon"/>
+      <a href="https://www.linkedin.com/in/vanessa-liaw-b0a1981a2/" target="_blank" rel="noreferrer" >
+        <FontAwesomeIcon icon={faLinkedin} className="icon"/>
       </a>
-      <a href="https://stackoverflow.com/users/19628967/vliaw">
-        <FontAwesomeIcon icon={faStackOverflow} target="_blank" rel="noreferrer" className="icon"/>
+      <a href="https://stackoverflow.com/users/19628967/vliaw" target="_blank" rel="noreferrer" >
+        <FontAwesomeIcon icon={faStackOverflow} className="icon"/>
       </a>
     </footer>
   );
